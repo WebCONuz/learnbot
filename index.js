c2c81f671cad59125f6d99da77931888e65db1bf
WebCONuz <muxammadi0799@gmail.com>
2023-04-19 13:38:10 +0500

    Added simple keyboard

--- index.js
+++ index.js
@@ -1,37 +1,9 @@
 const TOKEN = "5839830044:AAH6_Ms3iae_ZL99_7U4QFvFKweTOoRlQTE";
 const chatId = 1251901085;
 
-// (async () => {
-//   //   let response = await fetch(`https://api.telegram.org/bot${TOKEN}/getMe`);
-//   let response = await fetch(`https://api.telegram.org/bot${TOKEN}/getUpdates`);
-//   let res = await response.json();
-//   console.log(res.result[0].message);
-// })();
-
-// (async () => {
-//   let response = await fetch(
-//     `https://api.telegram.org/bot${TOKEN}/sendMessage`,
-//     {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//       body: JSON.stringify({
-//         chat_id: chatId,
-//         text: "Salom Muxammadi. Photo: https://picsum.photos/400",
-//         // parse_mode: "HTML",
-//         disable_web_page_preview: false,
-//         reply_to_message_id: 9,
-//       }),
-//     }
-//   );
-//   let res = await response.json();
-//   console.log(res);
-// })();
-
 (async () => {
   let response = await fetch(
-    `https://api.telegram.org/bot${TOKEN}/sendDocument`,
+    `https://api.telegram.org/bot${TOKEN}/sendMessage`,
     {
       method: "POST",
       headers: {
@@ -39,8 +11,28 @@ const chatId = 1251901085;
       },
       body: JSON.stringify({
         chat_id: chatId,
-        document: "https://picsum.photos/300",
-        caption: "Dengiz",
+        text: "Salom Muxammadi. Photo: https://picsum.photos/300",
+        disable_web_page_preview: false,
+        reply_to_message_id: 9,
+        reply_markup: {
+          one_time_keyboard: true,
+          resize_keyboard: true,
+          keyboard: [
+            [
+              {
+                text: "1-knopka",
+              },
+              {
+                text: "2-knopka",
+              },
+            ],
+            [
+              {
+                text: "3-knopka",
+              },
+            ],
+          ],
+        },
       }),
     }
   );
